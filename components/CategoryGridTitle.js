39da9d0b9cf3727d5d7c2eb59aad23ee777482f5
UzairKhan313 <uzairkhaan2003@gmail.com>
2023-12-13 14:17:26 +0000

    Styling Category Grid and Screen

---
+++ components/CategoryGridTitle.js
@@ -0,0 +1,49 @@
+import { StyleSheet, View, Pressable, Text, Platform } from "react-native";
+export const CategoryGridTitle = ({ title, color }) => {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        android_ripple={{ color: "#ccc" }}
+      >
+        <View style={[styles.innerContainer,{backgroundColor:color} ]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: "white", // for IOS to set border shadow background color must be set.
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 2 },
+    overflow: Platform.select({ android: "hidden", ios: "visible" }),
+  },
+  button: { flex: 1 },
+  buttonPressed: {
+    opacity: 0.25,
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+    // padding:16
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+});
