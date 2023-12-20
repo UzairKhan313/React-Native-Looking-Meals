b42791aa8606d3cead7d257b83c0669ec4120030
UzairKhan313 <uzairkhaan2003@gmail.com>
2023-12-20 13:02:59 +0000

    writting Context Logic

--- store/context/fovurites-context.js
+++ store/context/fovurites-context.js
@@ -6,8 +6,28 @@ const FavoritesContext = createContext({
   removeFavorite: (id) => {},
 });
 const favoriteContextProvider = ({ children }) => {
+  const [favoritesMealsIds, setFavoriteMealsIds] = useState([]);
 
+  const addFavorite = (id) => {
+    setFavoriteMealsIds((preFavIds) => [...preFavId, id]);
+  };
 
-  return <FavoritesContext.Provider>{children}</FavoritesContext.Provider>;
+  const removeFavorite = (id) => {
+    setFavoriteMealsIds((currFavIds) => {
+      currFavIds.filter((currFavId) => currFavId !== id);
+    });
+  };
+
+  const value = {
+    ids: favoritesMealsIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
 };
 export default favoriteContextProvider;
